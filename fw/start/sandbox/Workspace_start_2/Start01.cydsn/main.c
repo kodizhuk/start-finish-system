cdfd26ccd34f8ba48bb4c81692ceead2b9b4d942
Petro Zhuk <petro.zhuk.95@gmail.com>
2016-05-18 13:57:57 +0300

    add comment in program

--- fw/start/sandbox/Workspace_start_2/Start01.cydsn/main.c
+++ fw/start/sandbox/Workspace_start_2/Start01.cydsn/main.c
@@ -1,40 +1,53 @@
 #include <project.h>
-#include <stdio.h>      //include for sprintf
+#include <stdio.h>     
+#include <stdbool.h>
 
-#define MAXSKIER 9     //max number skier
-#define TEST_MODE_WIRED 0 // do we want to enable the test mode?
+/* max number skier */
+#define MAXSKIER 9    
+
+/* do we want to enable the test mode? */
+#define TEST_MODE_WIRED 0 
 
 /*Commands*/
 #define COMMAND_START   's'
 #define COMMAND_CANCEL  'c'
 #define COMMAND_READ    'r'
 
+/*led indication*/
+#define LED_ON  0
+#define LED_OFF 1
+
 /* Api to read (WDT) counter value
 * WDT Counter 0 Read value 
 * (uint32) CySysWdtGetCount(0u);  
 */
 
-void led_indication(void);
-void display_indication(int numSkier, int numSkiers);
-void outToDisplay(int numSkier, int position);
-void writeAllTime(char c);
+void LedIndication(void);
+void DisplayIndication(int numSkier, int numSkiers);
+void OutToDisplay(int numSkier, int position);
+void WriteAllTime(char data);
   
-typedef enum {false, true} bool;
+/* Structure for have current skyer time */
 struct time{
     uint16_t hour;
     uint16_t min;
     uint16_t sec;
     uint16_t msec;
 };
+
 /*data for each skier*/
 struct time tSkier[9] = {0,0,0,0};       
 
-bool startPress = false;
-bool cancelPress = false;    //flags for press button
+/*flags button */
+bool startPress = false;    
+bool cancelPress = false; 
 
-uint32_t skier = 0;      //number sportsmen last started
+/*number sportsmen last started*/
+uint32_t skier = 0;
 
-char string_buff[16];   //buffer for output string
+/*buffer for output string*/
+char string_buff[16];   
+
 
 /*interrupt for start button*/
 CY_ISR(startHandler)        
@@ -53,7 +66,7 @@ CY_ISR(startHandler)
         /*end of test code*/
         #endif
         
-        // LED indication
+        /* LED indication*/
         startPress = true;
     }
     start_ClearInterrupt(); 
@@ -64,7 +77,8 @@ CY_ISR(dataHandler)
 {
     uint8_t c;
     
-    writeAllTime(c);       //record time skiers 
+    /*record time skiers */
+    writeAllTime(c);      
 }
 
 /*interrupt for cancel button*/
@@ -74,19 +88,22 @@ CY_ISR(cancelHandler)
         /*put simbol cancel and number for skier*/
         sprintf(string_buff,"%c%c",COMMAND_CANCEL,skier+'0');
         xbee_PutString(string_buff);
+        
         skier--;
+        /*LED indication*/
         cancelPress = true;
     }
     cancel_ClearInterrupt();
 }
+
 /*interrupt for blink led*/
 CY_ISR(blincHandler)      
 {
     /*if skier!=0 start blinc LED*/
     if(skier){
-        led_blue_Write(0);
+        led_blue_Write(LED_ON);
         CyDelay(1);
-        led_blue_Write(1);
+        led_blue_Write(LED_OFF);
     }
     CySysWdtClearInterrupt(CY_SYS_WDT_COUNTER0_INT);
 }
@@ -94,26 +111,30 @@ CY_ISR(blincHandler)
 
 int main(void)
 {
+    uint32 interruptState;
     CyGlobalIntEnable;    
-    xbee_Start();                       //start uart for xbee
-    display_Start();                    //start lcd display
+    
+    interruptState = CyEnterCriticalSection();
+    xbee_Start();                       
+    display_Start();                    
+    CyExitCriticalSection(interruptState);
         
-    int_blinc_StartEx(blincHandler);    //interrupt for blink led
-    int_inputData_StartEx(dataHandler); //interrupt for RX data input
-    int_start_StartEx(startHandler);    //interrupt start button
-    int_cancel_StartEx(cancelHandler);  //interrupt cancel button
+    int_blinc_StartEx(blincHandler);    
+    int_inputData_StartEx(dataHandler);
+    int_start_StartEx(startHandler);  
+    int_cancel_StartEx(cancelHandler);
     
     
     while(1)
     {  
-        led_indication();
-        display_indication(1,2);
+        LedIndication();
+        DisplayIndication(1,2);
         CyDelay(1000);
     }
 }
 
 
-void led_indication()
+void LedIndication()
 {
     int i;
     
@@ -126,15 +147,15 @@ void led_indication()
     if(cancelPress)
         for(i=0;i<10;i++){       //func blinc cancel red led
             cancelPress = false;
-            led_blue_Write(1);
+            led_blue_Write(LED_OFF);
             led_red_Write(~led_red_Read());
             CyDelay(50);
         }
-    led_blue_Write(1);   
+    led_blue_Write(LED_OFF);   
 }
 
 /*output to display a specified time skier*/
-void display_indication(int numSkier, int numSkiers)
+void DisplayIndication(int numSkier, int numSkiers)
 {      
     int i;
     int position=0;
@@ -142,12 +163,26 @@ void display_indication(int numSkier, int numSkiers)
     for(i=0; i < numSkiers; i++,position++)
     {
         if(position > 1)position = 0;
-        outToDisplay(numSkier+i, position);
+        OutToDisplay(numSkier+i, position);
     }
 }
 
-/*function output to display*/
-void outToDisplay(int num, int position)
+/*******************************************************
+* Function name: outToDisplay
+*
+* Function does print the skyer 
+* results on display
+*
+* Parameters:
+* num - skyer number. Valid range - [0-9]
+* position - raw position where skyer results will be 
+* printed on display 
+*
+* return - if void is returned thhis line could be 
+* deleted.
+*
+*******************************************************/
+void OutToDisplay(int num, int position)
 {
     display_Position(position,0);
     sprintf(string_buff,"%i - %d:%d:%d:%d",
@@ -155,7 +190,8 @@ void outToDisplay(int num, int position)
     display_PrintString(string_buff);
 }
 
-void writeAllTime(char c)
+void WriteAllTime(char data)
 {
-        //if((c>='0' && c<='9') || c==':') tSkier[i++].min = c;
+        
+    //if((c>='0' && c<='9') || c==':') tSkier[i++].min = c;
 }
