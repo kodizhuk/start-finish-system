5fc5b62d28047a4a47bd755e98382e3c53bac0d1
Petro Zhuk <petro.zhuk.95@gmail.com>
2016-06-09 12:21:05 +0300

    added to the main file start and finish

---
+++ vesion 2/fw/finish/sandbox/main.c
@@ -0,0 +1,19 @@
+
+int main(void)
+{
+	/**/
+	bool controlStartPermissionSkier(void);
+	
+	/**/
+	void indicationStartAllowed(void);
+	
+	/*returns the next empty memory cell */
+	int writeRealTimeFinishSkier(uint16_t numberSkier);
+	
+	/**/
+	bool syncDataWithStart(struct SKIERRESULT *skierTime);
+	
+	/**/
+	int readCommandcontrolPanel(void);
+	
+}
---
+++ vesion 2/fw/start/sandbox/main.c
@@ -0,0 +1,22 @@
+
+int main(void)
+{
+	/**/
+	bool controlStartPermissionSkier(void);
+	
+	/**/
+	void indicationStartAllowed(void);
+	
+	/**/
+	void displayIndicationStartAllowed(void);
+	
+	/*returns the next empty memory cell */
+	int writeRealTimeSkier(uint16_t numberSkier);
+	
+	/**/
+	bool syncDataWithFinish(struct SKIERRESULT *skierTime);
+	
+	/**/
+	int readCommandcontrolPanel(void);
+	
+}
